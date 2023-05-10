a273839774bdc8344d56f4a13d0f126a07a7bb85
AsimSawafta <118603012+AsimSawafta@users.noreply.github.com>
2023-05-10 16:15:30 +0300

    hollo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About/About.jsx';
+import Home from './components/Home.jsx';
+
+function App() {
+  return (
+    <div>
+    <About />
+    <Home />
+    </div>
+  );
+}
+
+export default App;
